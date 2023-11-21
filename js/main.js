736c5f490c7659aac38a0078d977679ef6763e9b
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-21 17:59:32 +0100

    feat: foreach img

--- js/main.js
+++ js/main.js
@@ -1,64 +1,5 @@
 'use strict';
 
-//funzioni
-function nextSlide() {
-  domItems[currentItem].classList.remove('active');
-
-  if (currentItem === domItems.length - 1) {
-    currentItem = 0;
-  } else {
-    currentItem++;
-  }
-
-  domItems[currentItem].classList.add('active');
-}
-
-//variabili per gli elementi nel DOM
-const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
-const items = document.querySelector('.items');
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-
-let currentItem = 0;
-
-for (let i = 0; i < images.length; i++) {
-  const item = document.createElement('div');
-  item.classList.add('item');
-
-  //contenitori delle mie immagini
-  if (i === currentItem) {
-    item.classList.add('active');
-  }
-
-  //immagini
-  const img = document.createElement('img');
-  img.src = `img/${images[i]}`;
-  img.alt = `Random ${i}`;
-
-  item.append(img);
-  items.append(item);
-}
-
-const domItems = document.querySelectorAll('.item');
-
-//frecciette di top e bottom
-prev.addEventListener('click', function () {
-  domItems[currentItem].classList.remove('active');
-
-  if (currentItem === 0) {
-    currentItem = domItems.length - 1;
-  } else {
-    currentItem--;
-  }
-
-  domItems[currentItem].classList.add('active');
-});
-
-next.addEventListener('click', nextSlide);
-
-//autoplay
-setInterval(nextSlide, 3_000);
-
 /*Milestone 0:
 Creare un array di oggetti composto da Url, titolo, descrizione (potete inventare titolo e descrizione).
 */
@@ -90,3 +31,66 @@ const carouselImages = [
       'Fatti coccolare con il sole e la brezza di questa bellissima spiaggio tropicale',
   },
 ];
+
+//funzioni
+function nextSlide() {
+  domItems[currentItem].classList.remove('active');
+
+  if (currentItem === domItems.length - 1) {
+    currentItem = 0;
+  } else {
+    currentItem++;
+  }
+
+  domItems[currentItem].classList.add('active');
+}
+
+//variabili per gli elementi nel DOM
+const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
+const items = document.querySelector('.items');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+let currentItem = 0;
+
+/*Milestone 1:
+Usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+*/
+carouselImages.forEach((item, i) => {
+  const elemento = document.createElement('div');
+  elemento.classList.add('item');
+
+  if (i === 0) {
+    elemento.classList.add('active');
+  }
+  console.log(i);
+
+  //immagini
+  const img = document.createElement('img');
+  img.src = `img/${item.url}`;
+  img.alt = `Random ${item.url}`;
+
+  elemento.append(img);
+  items.append(elemento);
+});
+
+const domItems = document.querySelectorAll('.item');
+
+//frecciette di top e bottom
+prev.addEventListener('click', function () {
+  domItems[currentItem].classList.remove('active');
+
+  if (currentItem === 0) {
+    currentItem = domItems.length - 1;
+  } else {
+    currentItem--;
+  }
+
+  domItems[currentItem].classList.add('active');
+});
+
+next.addEventListener('click', nextSlide);
+
+//autoplay
+setInterval(nextSlide, 3_000);
